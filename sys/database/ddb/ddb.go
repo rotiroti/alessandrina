14759136e66169c83c6bd6ed2b3f62594435b25f
Marco Rotiroti <marco.rotiroti@gmail.com>
2023-07-04 20:25:12 +0200

    feat: provide support for saving new books inside DynamoDB

---
+++ sys/database/ddb/ddb.go
@@ -0,0 +1,53 @@
+package ddb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/rotiroti/alessandrina/domain"
+)
+
+// DynamodbAPI is the interface used to interact with AWS DynamoDB.
+//
+//go:generate mockery --name DynamoDB
+type DynamodbAPI interface {
+	PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error)
+}
+
+// Store is a DynamoDB implementation of the Storer interface.
+type Store struct {
+	api       DynamodbAPI
+	tableName string
+}
+
+var _ domain.Storer = (*Store)(nil)
+
+// NewStore returns a new instance of Store.
+func NewStore(tableName string, api DynamodbAPI) *Store {
+	return &Store{
+		tableName: tableName,
+		api:       api,
+	}
+}
+
+// Save adds a new book into the DynamoDB database.
+func (s *Store) Save(ctx context.Context, book domain.Book) error {
+	item, err := attributevalue.MarshalMap(ToDynamodbBook(book))
+	if err != nil {
+		return fmt.Errorf("marshalmap: %w", err)
+	}
+
+	_, err = s.api.PutItem(ctx, &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String(s.tableName),
+	})
+
+	if err != nil {
+		return fmt.Errorf("putitem: %w", err)
+	}
+
+	return nil
+}
