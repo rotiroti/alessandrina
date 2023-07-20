1f9a2cd3907c8ce51f0f3bbe02f144686d6eedf6
Marco Rotiroti <marco.rotiroti@gmail.com>
2023-07-20 14:58:30 +0200

    refactor: instantiate storage using a custom configuration

--- sys/database/ddb/ddb_test.go
+++ sys/database/ddb/ddb_test.go
@@ -15,17 +15,55 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func setup(t *testing.T) (*ddb.Store, *ddb.MockDynamodbAPI, string, uuid.UUID) {
-	table := "test-table"
-	client := ddb.NewMockDynamodbAPI(t)
-	store := ddb.NewStore(table, client)
+func setup(t *testing.T) (*ddb.Store, ddb.Config, uuid.UUID) {
+	conf := ddb.Config{TableName: "test-table"}
+	store, err := ddb.NewStore(context.Background(), conf)
+
+	require.NoError(t, err)
+
 	bookID := uuid.MustParse("ad8b59c2-5fe6-4267-b0cf-6d2f9eb1c812")
 
-	return store, client, table, bookID
+	return store, conf, bookID
+}
+
+func TestNewStore(t *testing.T) {
+	tests := []struct {
+		name    string
+		conf    ddb.Config
+		wantErr bool
+	}{
+		{
+			name:    "EmptyTableName",
+			conf:    ddb.Config{},
+			wantErr: true,
+		},
+		{
+			name: "OK",
+			conf: ddb.Config{
+				TableName: "test-table",
+				ClientLog: "true",
+				Endpoint:  "http://localhost:8080",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ddb.NewStore(context.Background(), tt.conf)
+
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
 }
 
 func TestStore(t *testing.T) {
-	store, client, table, bookID := setup(t)
+	store, conf, bookID := setup(t)
+	client := ddb.NewMockDynamodbAPI(t)
 	ctx := context.Background()
 	expectedBook := domain.Book{
 		ID:        bookID,
@@ -36,22 +74,22 @@ func TestStore(t *testing.T) {
 		ISBN:      "978-0-261-10235-4",
 	}
 	expectedPutItemInput := &dynamodb.PutItemInput{
-		TableName: aws.String(table),
+		TableName: aws.String(conf.TableName),
 	}
 	expectedKey := map[string]types.AttributeValue{
 		"id": &types.AttributeValueMemberS{Value: bookID.String()},
 	}
 	expectedGetItemInput := &dynamodb.GetItemInput{
 		Key:       expectedKey,
-		TableName: aws.String(table),
+		TableName: aws.String(conf.TableName),
 	}
 	expectedDeleteInput := &dynamodb.DeleteItemInput{
 		Key:                 expectedKey,
-		TableName:           aws.String(table),
+		TableName:           aws.String(conf.TableName),
 		ConditionExpression: aws.String("attribute_exists(id)"),
 	}
 	expectedScanInput := dynamodb.ScanInput{
-		TableName: aws.String(table),
+		TableName: aws.String(conf.TableName),
 		Limit:     aws.Int32(ddb.DefaultTableScanLimit),
 	}
 
@@ -60,6 +98,7 @@ func TestStore(t *testing.T) {
 		require.NoError(t, err)
 		expectedPutItemInput.Item = saveBookItem
 		client.EXPECT().PutItem(ctx, expectedPutItemInput).Return(nil, nil).Once()
+		store.Client = client
 		err = store.Save(ctx, expectedBook)
 		require.NoError(t, err)
 		client.AssertExpectations(t)
@@ -70,6 +109,7 @@ func TestStore(t *testing.T) {
 		require.NoError(t, err)
 		expectedPutItemInput.Item = saveBookItem
 		client.EXPECT().PutItem(ctx, expectedPutItemInput).Return(&dynamodb.PutItemOutput{}, assert.AnError).Once()
+		store.Client = client
 		err = store.Save(ctx, expectedBook)
 		require.Error(t, err)
 		client.AssertExpectations(t)
@@ -89,6 +129,7 @@ func TestStore(t *testing.T) {
 			},
 		}
 		client.EXPECT().Scan(ctx, &expectedScanInput).Return(expectedScanOutput, nil).Once()
+		store.Client = client
 		books, err := store.FindAll(ctx)
 		require.NoError(t, err)
 		require.Equal(t, expectedBooks, books)
@@ -97,6 +138,7 @@ func TestStore(t *testing.T) {
 
 	t.Run("FindAllFail", func(t *testing.T) {
 		client.EXPECT().Scan(ctx, &expectedScanInput).Return(&dynamodb.ScanOutput{}, assert.AnError).Once()
+		store.Client = client
 		_, err := store.FindAll(ctx)
 		require.Error(t, err)
 		client.AssertExpectations(t)
@@ -105,12 +147,15 @@ func TestStore(t *testing.T) {
 	t.Run("FindOne", func(t *testing.T) {
 		getItemOutput, err := attributevalue.MarshalMap(ddb.ToDynamodbBook(expectedBook))
 		require.NoError(t, err)
+
 		client.EXPECT().GetItem(ctx, expectedGetItemInput).Return(
 			&dynamodb.GetItemOutput{
 				Item: getItemOutput,
 			},
 			nil,
 		).Once()
+
+		store.Client = client
 		foundBook, err := store.FindOne(ctx, bookID)
 		require.NoError(t, err)
 		assert.Equal(t, expectedBook, foundBook)
@@ -119,6 +164,7 @@ func TestStore(t *testing.T) {
 
 	t.Run("FindOneFail", func(t *testing.T) {
 		client.EXPECT().GetItem(ctx, expectedGetItemInput).Return(&dynamodb.GetItemOutput{}, assert.AnError).Once()
+		store.Client = client
 		_, err := store.FindOne(ctx, bookID)
 		require.Error(t, err)
 		client.AssertExpectations(t)
@@ -126,6 +172,7 @@ func TestStore(t *testing.T) {
 
 	t.Run("FindOneItemNotFound", func(t *testing.T) {
 		client.EXPECT().GetItem(ctx, expectedGetItemInput).Return(&dynamodb.GetItemOutput{Item: map[string]types.AttributeValue{}}, nil).Once()
+		store.Client = client
 		_, err := store.FindOne(ctx, bookID)
 		require.Error(t, err)
 		client.AssertExpectations(t)
@@ -133,12 +180,14 @@ func TestStore(t *testing.T) {
 
 	t.Run("Delete", func(t *testing.T) {
 		client.EXPECT().DeleteItem(ctx, expectedDeleteInput).Return(nil, nil).Once()
+		store.Client = client
 		err := store.Delete(ctx, bookID)
 		require.NoError(t, err)
 		client.AssertExpectations(t)
 	})
 	t.Run("DeleteFail", func(t *testing.T) {
 		client.EXPECT().DeleteItem(ctx, expectedDeleteInput).Return(&dynamodb.DeleteItemOutput{}, assert.AnError).Once()
+		store.Client = client
 		err := store.Delete(ctx, bookID)
 		require.Error(t, err)
 		client.AssertExpectations(t)
--- tests/integration/dynamodb_test.go
+++ tests/integration/dynamodb_test.go
@@ -5,9 +5,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/google/uuid"
 	"github.com/rotiroti/alessandrina/domain"
@@ -22,185 +19,87 @@ func skipLocalstack(t *testing.T) {
 	}
 }
 
-func setupLocalstack(t *testing.T) (tableName string, client *dynamodb.Client) {
+func setupDB(t *testing.T) (*ddb.Store, uuid.UUID) {
 	t.Helper()
 
-	ctx := context.Background()
-	tableName, ok := os.LookupEnv("TABLE_NAME")
-	if !ok || tableName == "" {
-		t.Fatalf("TABLE_NAME environment variable not set")
+	conf := ddb.Config{
+		TableName: os.Getenv("TABLE_NAME"),
+		Endpoint:  os.Getenv("AWS_ENDPOINT"),
+		ClientLog: os.Getenv("AWS_CLIENT_DEBUG"),
 	}
 
-	customResolver := aws.EndpointResolverWithOptionsFunc(
-		func(_, region string, options ...interface{}) (aws.Endpoint, error) {
-			endpoint := "http://localstack_main:4566"
+	store, err := ddb.NewStore(context.Background(), conf)
+	require.NoError(t, err)
 
-			return aws.Endpoint{
-				PartitionID:       "aws",
-				URL:               endpoint,
-				SigningRegion:     region,
-				HostnameImmutable: true,
-			}, nil
-		})
+	bookID := uuid.MustParse("ad8b59c2-5fe6-4267-b0cf-6d2f9eb1cabd")
 
-	// Enable debug logging to see the HTTP requests and responses bodies.
-	var logMode aws.ClientLogMode
-
-	if os.Getenv("AWS_CLIENT_DEBUG") == "true" {
-		logMode |= aws.LogRequestWithBody | aws.LogResponseWithBody
-	}
-
-	options := []func(*config.LoadOptions) error{
-		config.WithEndpointResolverWithOptions(customResolver),
-		config.WithClientLogMode(logMode),
-	}
-
-	conf, err := config.LoadDefaultConfig(ctx, options...)
-	if err != nil {
-		t.Fatalf("failed to load SDK configuration, %v", err)
-	}
-
-	client = dynamodb.NewFromConfig(conf)
-
-	return tableName, client
+	return store, bookID
 }
 
 func TestDynamodbFlow(t *testing.T) {
 	// Skip the DynamoDB test if the LOCALSTACK environment variable is not set
 	skipLocalstack(t)
 
-	// Setup test environment
 	ctx := context.Background()
-	tableName, client := setupLocalstack(t)
+	store, expectedBookID := setupDB(t)
+	expectedBook := domain.Book{
+		ID:        expectedBookID,
+		Title:     "The Lord of the Rings",
+		Authors:   "J.R.R. Tolkien",
+		Pages:     1178,
+		Publisher: "George Allen & Unwin",
+		ISBN:      "978-0-261-10235-4",
+	}
 
 	t.Run("Save", func(t *testing.T) {
-		// Create a new book
-		bookID := uuid.MustParse("ad8b59c2-5fe6-4267-b0cf-6d2f9eb1c812")
-		book := domain.Book{
-			ID:        bookID,
-			Title:     "The Lord of the Rings",
-			Authors:   "J.R.R. Tolkien",
-			Pages:     1178,
-			Publisher: "George Allen & Unwin",
-			ISBN:      "978-0-261-10235-4",
-		}
-
-		// Create a new store using the Localstack DynamoDB instance
-		//
-		// NOTE: the table name should coincide with the one created
-		//       by using the `scripts/create-table` script
-		store := ddb.NewStore(tableName, client)
-
-		// Call the Save method of the store
-		err := store.Save(ctx, book)
-
-		// Assert the expected output
+		err := store.Save(ctx, expectedBook)
 		require.NoError(t, err)
 	})
 
 	t.Run("SaveTableNotExist", func(t *testing.T) {
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore("not-exist", client)
+		conf := ddb.Config{TableName: "not-exist"}
+		store, err := ddb.NewStore(context.Background(), conf)
 
-		// Call the Save method of the store
-		err := store.Save(ctx, domain.Book{})
+		require.NoError(t, err)
 
-		// Assert the expected output
+		err = store.Save(ctx, domain.Book{})
 		require.Error(t, err)
 	})
 
 	t.Run("FindOne", func(t *testing.T) {
-		// Create a new book
-		bookID := uuid.MustParse("ad8b59c2-5fe6-4267-b0cf-6d2f9eb1cabd")
-		expectedBook := domain.Book{
-			ID:        bookID,
-			Title:     "The Lord of the Rings",
-			Authors:   "J.R.R. Tolkien",
-			Pages:     1178,
-			Publisher: "George Allen & Unwin",
-			ISBN:      "978-0-261-10235-4",
-		}
-
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore(tableName, client)
-
-		// Call the Save method of the store
 		err := store.Save(ctx, expectedBook)
 
-		// Assert the expected output
 		require.NoError(t, err)
 
-		// Call the FindOne method of the store
-		book, err := store.FindOne(ctx, bookID)
+		book, err := store.FindOne(ctx, expectedBookID)
 
-		// Assert the expected output
 		require.NoError(t, err)
-
-		// Assert the expected output
 		require.Equal(t, expectedBook, book)
 	})
 
 	t.Run("FindOneBookNotFound", func(t *testing.T) {
-		// Create a new book
-		bookID := uuid.MustParse("01234567-0123-0123-0123-0123456789ab")
+		_, err := store.FindOne(ctx, uuid.MustParse("01234567-0123-0123-0123-0123456789ab"))
 
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore(tableName, client)
-
-		// Call the FindOne method of the store
-		_, err := store.FindOne(ctx, bookID)
-
-		// Assert the expected output
 		require.Error(t, err)
 	})
 
 	t.Run("Delete", func(t *testing.T) {
-		// Create a new book
-		bookID := uuid.MustParse("ad8b59c2-5fe6-4267-b0cf-6d2f9eb1cabd")
-		expectedBook := domain.Book{
-			ID:        bookID,
-			Title:     "The Lord of the Rings",
-			Authors:   "J.R.R. Tolkien",
-			Pages:     1178,
-			Publisher: "George Allen & Unwin",
-			ISBN:      "978-0-261-10235-4",
-		}
-
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore(tableName, client)
-
-		// Call the Save method of the store
 		err := store.Save(ctx, expectedBook)
 
-		// Assert the expected output
 		require.NoError(t, err)
 
-		// Call the Delete method of the store
-		err = store.Delete(ctx, bookID)
+		err = store.Delete(ctx, expectedBookID)
 
-		// Assert the expected output
 		require.NoError(t, err)
 	})
 
 	t.Run("DeleteBookNotFound", func(t *testing.T) {
-		// Create a new book
-		bookID := uuid.MustParse("01234567-0123-0123-0123-0123456789ab")
+		err := store.Delete(ctx, uuid.MustParse("01234567-0123-0123-0123-0123456789ab"))
 
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore(tableName, client)
-
-		// Call the Delete method of the store
-		err := store.Delete(ctx, bookID)
-
-		// Assert the expected output
 		require.Error(t, err)
 	})
 
 	t.Run("FindAllBooks", func(t *testing.T) {
-		// Create a new store using the Localstack DynamoDB instance
-		store := ddb.NewStore(tableName, client)
-
-		// Seed the database with some books
 		newBooksLen := 3
 		newBooks := make([]domain.Book, newBooksLen)
 
@@ -220,7 +119,6 @@ func TestDynamodbFlow(t *testing.T) {
 			newBooks[i] = book
 		}
 
-		// Call the FindAll method of the store
 		books, err := store.FindAll(ctx)
 
 		// Assert the expected output
